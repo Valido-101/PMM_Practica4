43fc708a56d80ad78cdd464c6f25db105766988b
Jesús Valido Zafra <jesuszafra22@gmail.com>
2020-12-22 12:46:06 +0100

    Comentarios añadidos a Fragmento_musica

--- app/src/main/java/com/example/practica_4/Fragmento_musica.java
+++ app/src/main/java/com/example/practica_4/Fragmento_musica.java
@@ -27,19 +27,30 @@ import java.util.concurrent.TimeUnit;
  */
 public class Fragmento_musica extends Fragment {
 
+    //Vistas necesarias
     private Button b1,b2,b3,b4;
     private ImageView iv;
+
+    //Objeto necesario para reproducir archivos de audio grandes
     private MediaPlayer mediaPlayer;
 
+    //Variables que llevan el control del tiempo de inicio y el tiempo final
     private double startTime = 0;
     private double finalTime = 0;
 
+    //Variable que nos ayudará a que la canción se reproduzca en el punto en el que pongamos el thumb del seekbar
     private int posicion;
 
+    //Handler (hilo secundario)
     private Handler myHandler = new Handler();;
+    //Tiempos de avance y de retroceso (5 segundos)
     private int forwardTime = 5000;
     private int backwardTime = 5000;
+
+    //Seekbar para controlar el punto de reproducción
     private SeekBar seekbar;
+
+    //TextViews que muestran tiempo de reproducción actual y total de reproducción
     private TextView tx1,tx2;
     public static int oneTimeOnly = 0;
 
@@ -56,6 +67,7 @@ public class Fragmento_musica extends Fragment {
      * @return A new instance of fragment Fragmento_pelota.
      */
     // TODO: Rename and change types and number of parameters
+    //Método para instanciar el fragmento
     public static Fragmento_musica newInstance() {
         Fragmento_musica fragment = new Fragmento_musica();
         return fragment;
@@ -66,6 +78,7 @@ public class Fragmento_musica extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    //Método que se ejecuta al crear una vista. La inflamos con el inflater
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -73,10 +86,13 @@ public class Fragmento_musica extends Fragment {
         return view;
     }
 
+    //Método que se ejecuta cuando se ha creado una vista (se asegura de que está creada). Comportamiento de los botones
+    //y todo lo necesario para reproducir la música
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //Obtenemos todos los botones y los textviews
         b1 = (Button) getView().findViewById(R.id.btn_play);
         b2 = (Button) getView().findViewById(R.id.btn_pause);
         b3 = (Button) getView().findViewById(R.id.btn_forward_5secs);
@@ -84,12 +100,18 @@ public class Fragmento_musica extends Fragment {
         tx1 = (TextView) getView().findViewById(R.id.txt1);
         tx2 = (TextView) getView().findViewById(R.id.txt2);
 
+        //Inicializamos el mediaplayer con la canción deseada
         mediaPlayer = MediaPlayer.create(getContext(), R.raw.pokemon_gym);
+        //Obtenemos el seekbar
         seekbar = (SeekBar)getView().findViewById(R.id.seekbar);
         seekbar.setClickable(false);
+        //Como la canción comienza en estado de pausa, inhabilitamos el botón de pausa
         b2.setEnabled(false);
 
+        //Establecemos un OnSeekBarChangeListener al seekbar para poder reproducir la canción donde soltemos el thumb
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            //Cada vez que se actualice la posición del seekbar, guardaremos esa posición, ya sea automáticamente
+            //o porque el usuario lo mueva
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 posicion=progress;
@@ -100,18 +122,24 @@ public class Fragmento_musica extends Fragment {
 
             }
 
+            //Cuando coge el thumb, lo arrastra y lo suelta, la canción se reproduce en ese punto
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 mediaPlayer.seekTo(posicion);
             }
         });
 
+        //Botón de reproducir, asignamos el listener y su comportamiento
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Mostramos el toast diciendo que se ha empezado a reproducir la canción
                 Toast.makeText(getContext(), "Playing sound",Toast.LENGTH_SHORT).show();
-                        mediaPlayer.start();
 
+                //Empezamos a reproducirla
+                mediaPlayer.start();
+
+                //Establecemos la duración total e inicial
                 finalTime = mediaPlayer.getDuration();
                 startTime = mediaPlayer.getCurrentPosition();
 
@@ -120,6 +148,7 @@ public class Fragmento_musica extends Fragment {
                     oneTimeOnly = 1;
                 }
 
+                //Actualizamos el textview con los datos de tiempo total de reproducción
                 tx2.setText(String.format("%d min, %d sec",
                         TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
                         TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
@@ -127,6 +156,7 @@ public class Fragmento_musica extends Fragment {
                                         finalTime)))
                 );
 
+                //Actualizamos el textview con los datos de tiempo actual de reproducción
                 tx1.setText(String.format("%d min, %d sec",
                         TimeUnit.MILLISECONDS.toMinutes((long) startTime),
                         TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
@@ -134,64 +164,92 @@ public class Fragmento_musica extends Fragment {
                                         startTime)))
                 );
 
+                //Actualizamos el progreso del seekbar con el momento actual de reproducción
                 seekbar.setProgress((int)startTime);
+                //LLamamos al método de actualizar tiempo de canción una vez cada segundo en un hilo secundario
                 myHandler.postDelayed(UpdateSongTime,100);
+                //Habilitamos el botón de pausa
                 b2.setEnabled(true);
+                //Inhabilitamos el de reproducir
                 b1.setEnabled(false);
             }
         });
 
+        //OnClickListener y comportamiento del botón de pausa
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Informamos de que se ha pausado
                 Toast.makeText(getContext(), "Pausing sound",Toast.LENGTH_SHORT).show();
-                        mediaPlayer.pause();
+                //Pausamos canción
+                mediaPlayer.pause();
+                //Inhabilitamos el botón de pausa
                 b2.setEnabled(false);
+                //Habilitamos el de reproducir
                 b1.setEnabled(true);
             }
         });
 
+        //OnClickListener y comportamiento del botón de adelantar 5 segundos
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Creamos una variable con el punto actual de reproducción
                 int temp = (int)startTime;
 
+                //Si esa variable más los 5 segundos son inferiores al tiempo total de reproducción
                 if((temp+forwardTime)<=finalTime){
+                    //Se suma al tiempo actual los 5 segundos
                     startTime = startTime + forwardTime;
+                    //Reproducimos la canción en el punto que está 5 segundos después
                     mediaPlayer.seekTo((int) startTime);
+                    //Informamos
                     Toast.makeText(getContext(),"You have Jumped forward 5 seconds",Toast.LENGTH_SHORT).show();
                 }else{
+                    //Si no se puede se informa también
                     Toast.makeText(getContext(),"Cannot jump forward 5 seconds",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+        //OnClickListener y comportamiento del botón de retrasar 5 segundos
         b4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Creamos una variable con el punto actual de reproducción
                 int temp = (int)startTime;
 
+                //Si esa variable menos los 5 segundos son superiores a 0
                 if((temp-backwardTime)>0){
+                    //Se resta al tiempo actual los 5 segundos
                     startTime = startTime - backwardTime;
+                    //Reproducimos la canción en el punto que está 5 segundos antes
                     mediaPlayer.seekTo((int) startTime);
+                    //Informamos
                     Toast.makeText(getContext(),"You have Jumped backward 5 seconds",Toast.LENGTH_SHORT).show();
                 }else{
+                    //Si no se puede se informa también
                     Toast.makeText(getContext(),"Cannot jump backward 5 seconds",Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
+    //Método que sirve para actualizar el seekbar y el tiempo de reproducción actual
     private Runnable UpdateSongTime = new Runnable() {
         public void run() {
+            //Igualamos el tiempo de inicio a la posición actual
             startTime = mediaPlayer.getCurrentPosition();
+            //Actualizamos el valor del textview con el tiempo de reproducción actual
             tx1.setText(String.format("%d min, %d sec",
                     TimeUnit.MILLISECONDS.toMinutes((long) startTime),
                     TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
                             TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
                                     toMinutes((long) startTime)))
             );
+            //Actualizamos el progreso del seekbar
             seekbar.setProgress((int)startTime);
+            //Ejecutamos el método una vez cada segundo
             myHandler.postDelayed(this, 100);
         }
     };
